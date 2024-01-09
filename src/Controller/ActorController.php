52133b440e29cf0055d55edf8ab8891fd0a97ea1
Charlotte GAERTNER <charlotte.gaertner@outlook.fr>
2024-01-09 11:43:49 +0100

    challenge 13

---
+++ src/Controller/ActorController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Actor;
+use App\Repository\ActorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActorController extends AbstractController
+{
+    #[Route('/actor', name: 'app_actor')]
+    public function index(ActorRepository $actorRepository): Response
+    {
+        return $this->render('actor/index.html.twig', [
+            'actors' => $actorRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/actor/{id<^\d+$>}', name: 'app_actor_show')]
+    public function show(Actor $actor): Response
+    {
+        return $this->render('actor/show.html.twig', [
+            'actor' => $actor,
+        ]);
+    }
+}
